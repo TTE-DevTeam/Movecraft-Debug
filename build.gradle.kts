ee0b37e1992c31d0d36dce0617e5d30f6e59b188
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-01-11 19:49:26 +0100

    fix gradle, it works now, now we can get started...

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,35 +2,43 @@ plugins {
     `java-library`
     id("java")
     id("io.github.0ffz.github-packages") version "1.2.1"
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.12"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.13"
 }
 
 group = "de.dertoaster"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
 repositories {
     gradlePluginPortal()
     mavenLocal()
     mavenCentral()
+    // Breaks paperweight
+    //maven {"https://repo.dmulloy2.net/repository/public/"}
+    ivy {
+        name = "Github Releases - ProtocolLib"; // Github Releases
+        url = uri("https://github.com");
+
+        patternLayout {
+            artifact("[organisation]/[module]/releases/download/[revision]/[module].[ext]");
+        }
+
+        metadataSources { artifact() }
+    }
+    maven { githubPackage("apdevteam/movecraft")(this) }
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven {"https://repo.dmulloy2.net/repository/public/"}
-    //maven { githubPackage("apdevteam/movecraft")(this) }
 }
 
 dependencies {
-    //api("org.jetbrains:annotations-java5:24.1.0")
+    api("org.jetbrains:annotations-java5:24.1.0")
+    //compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0") // ProtocolLib
+    implementation("dmulloy2:ProtocolLib:5.3.0@jar")
     paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0") // ProtocolLib
     compileOnly("net.countercraft:movecraft:+") // Movecraft dependency (latest version)
     compileOnly("it.unimi.dsi:fastutil:8.5.11")
 }
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-tasks.assemble {
-    dependsOn(tasks.reobfJar)
-}
