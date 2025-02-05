56b8989a264da5e3c634e8e9756d8a9f3a2e6453
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-02-05 09:44:03 +0100

    Update CraftUtil.java

--- src/main/java/de/dertoaster/movecraftdebug/CraftUtil.java
+++ src/main/java/de/dertoaster/movecraftdebug/CraftUtil.java
@@ -5,14 +5,21 @@ import net.countercraft.movecraft.craft.CraftManager;
 import net.countercraft.movecraft.craft.PlayerCraft;
 import net.countercraft.movecraft.craft.SubCraft;
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 public class CraftUtil {
 
-    public static Set<Craft> getRelevantCrafts(Player owner) {
-        PlayerCraft playerCraft = CraftManager.getInstance().getCraftByPlayer(owner);
+    public static Set<Craft> getRelevantCrafts(UUID owner) {
+        Player playerObj = Bukkit.getPlayer(owner);
+        if (playerObj == null) {
+            return Set.of();
+        }
+        // TODO: Add direct method to get craft by player UUID
+        PlayerCraft playerCraft = CraftManager.getInstance().getCraftByPlayer(playerObj);
         if (playerCraft == null) {
             return Set.of();
         }
