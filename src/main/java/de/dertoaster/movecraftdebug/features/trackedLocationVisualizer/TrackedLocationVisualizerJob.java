9eb99b6c1b70446d66d8fc29cbadd6257d85d13d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-02-05 09:40:24 +0100

    Use UUID instead of Player
    Prevents NPEs in library

--- src/main/java/de/dertoaster/movecraftdebug/features/trackedLocationVisualizer/TrackedLocationVisualizerJob.java
+++ src/main/java/de/dertoaster/movecraftdebug/features/trackedLocationVisualizer/TrackedLocationVisualizerJob.java
@@ -4,34 +4,45 @@ import de.dertoaster.movecraftdebug.CraftUtil;
 import net.countercraft.movecraft.craft.Craft;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.Bukkit;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.WeakHashMap;
 
 public class TrackedLocationVisualizerJob extends BukkitRunnable {
 
-    static final WeakHashMap<Player, VisualizerData> playerSettings = new WeakHashMap<>();
+    // TODO: Change to UUID instead of player!
+    static final WeakHashMap<UUID, VisualizerData> playerSettings = new WeakHashMap<>();
 
     public static VisualizerData getSettingsFor(final Player player) {
-        return playerSettings.computeIfAbsent(player, p -> {
-           return new VisualizerData(p);
+        return playerSettings.computeIfAbsent(player.getUniqueID(), p -> {
+           return new VisualizerData(p.getUniqueID().);
         });
     }
 
     public static void remove(final Player player) {
-        if (playerSettings.containsKey(player)) {
-            playerSettings.get(player).resetAllHighlights();
+        if (playerSettings.containsKey(player.getUniqueID())) {
+            final UUID originalUUID = player.getUniqueID();
+            playerSettings.get(new UUID(originalUUID.getMostSignificantBits(), originalUUID.getLeastSignificantBits())).resetAllHighlights();
         }
-        playerSettings.remove(player);
+        playerSettings.remove(player.getUniqueID());
     }
 
     @Override
-    public void run() {
+    public void run() {        
         playerSettings.entrySet().forEach(TrackedLocationVisualizerJob::update);
     }
 
-    private static void update(Map.Entry<Player, VisualizerData> playerVisualizerDataEntry) {
+    private static void update(Map.Entry<UUID, VisualizerData> playerVisualizerDataEntry) {
+        // Ignore offline or null players
+        Player playerObj = Bukkit.getPlayer(playerVisualizerDataEntry.getKey());
+        if (playerObj == null || !playerObj.isOnline()) {
+            // TODO: When the player is offline for too long, remove the mapping
+            return;
+        }
+        
         playerVisualizerDataEntry.getValue().resetAllHighlights();
         final Set<Craft> craftSet = CraftUtil.getRelevantCrafts(playerVisualizerDataEntry.getKey());
         if (craftSet.isEmpty()) {
